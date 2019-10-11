4b3802c11d6f2894e2b6e823ee129f5c44b2752e
Mathilde-Garin <mathilde.garin@hotmail.fr>
2019-10-11 14:42:47 +0200

    carousel final

--- carousel.js
+++ carousel.js
@@ -1,6 +1,5 @@
 var carousel = document.getElementById("carousel");
 var totalCarousel = carousel.children.length;
-var currentSlide = carousel.children[0];
 var index = 0;
 
 
@@ -13,24 +12,45 @@ prev = document.getElementById("precedent");
 
 
 function suivant() {  
-
-	carousel.children[index].style.display = "none";
-	index++;
-
-	for (i = 0 ; i < totalCarousel ; i++) {
-		if (index == carousel.children.length) {
-			index = 0;
-			carousel.children.style.display = "block";
-			carousel.children[index].style.display = "none";
-			index++;
+	
+	if (index >= 3) {
+		index = 0;				
+		for (var i = 0 ; i < totalCarousel ; i++) {
+			carousel.children[i].style.display = "inline-block";
 		}
-
 	}
 
-console.log(carousel.children[index]);
+	else {
+		carousel.children[index].style.display = "none";
+		index++;
+	}
 
+console.log(index);
 }
 
 
 next.onclick = suivant;
+
+
+function precedent() {
+
+	console.log(index);
+
+	if (index == 0) {
+		index = 3;
+		for (var i = 0 ; i < (totalCarousel-1) ; i++) {	
+			carousel.children[i].style.display = "none";
+		}
+	}
+
+	else {
+		index--;	
+		carousel.children[index].style.display = "inline-block";	
+	}
+
+	console.log(index);
+
+}
+
+
 prev.onclick = precedent;
